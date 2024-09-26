0daaef0f35be43268dde419a97c128fe9d036a4f
dongpyoyang <ydp1025@gmail.com>
2024-09-26 22:26:44 +0900

    Update custom_config.h

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -46,10 +46,10 @@ U_NP,              U_NP,              U_NA,              U_NA,              U_NA
      K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
      N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
 ) \
-XXX    K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  XXX \
-&kp LCTRL  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  &u_to_U_change_BASE \
-&kp LSHIFT K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  &u_to_U_change_EXTRA \
-                 K32  K33  K34       K35  K36  K37
+&u_to_U_MABI    K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  XXX \
+&kp LCTRL       K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  &u_to_U_change_BASE \
+&kp LSHIFT      K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  &u_to_U_change_EXTRA \
+                          K32  K33  K34       K35  K36  K37
 
 #define MIRYOKU_LAYER_LIST \
 MIRYOKU_X(BASE,   "Base") \
